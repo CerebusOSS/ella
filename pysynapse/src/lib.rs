345f9dd20edba91ca4ba9cc1d019cd0f65041575
Dexter Duckworth <dexterduck@users.noreply.github.com>
2023-06-02 12:36:33 -0400

    Started building out pysynapse package.

--- pysynapse/src/lib.rs
+++ pysynapse/src/lib.rs
@@ -1,6 +1,70 @@
+use futures::Future;
 use pyo3::prelude::*;
 
+use synapse::{Runtime, RuntimeConfig};
+
+#[cfg(feature = "mimalloc")]
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+
 #[pymodule]
-fn synapse(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+#[pyo3(name = "_internal")]
+fn pysynapse(_py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    m.add(
+        "runtime",
+        TokioRuntime(tokio::runtime::Runtime::new().unwrap()),
+    )?;
+    m.add_class::<PyRuntime>()?;
     Ok(())
+}
+
+#[derive(Debug, Clone)]
+#[pyclass(name = "RuntimeConfig")]
+pub(crate) struct PyRuntimeConfig {
+    pub(crate) cfg: RuntimeConfig,
+}
+
+#[pyclass(name = "Runtime")]
+pub(crate) struct PyRuntime {
+    pub(crate) rt: Runtime,
+}
+
+#[pymethods]
+impl PyRuntime {
+    #[pyo3(signature = (root, config=None))]
+    #[new]
+    fn new(py: Python, root: String, config: Option<PyRuntimeConfig>) -> anyhow::Result<Self> {
+        let config = if let Some(c) = config {
+            c.cfg
+        } else {
+            RuntimeConfig::default()
+        };
+        let rt = wait_for_future(py, Runtime::start_with_config(root, config))?;
+        Ok(Self { rt })
+    }
+
+    fn shutdown(&self, py: Python) -> anyhow::Result<()> {
+        Ok(wait_for_future(py, self.rt.shutdown())?)
+    }
+}
+
+#[pyclass]
+pub struct TokioRuntime(tokio::runtime::Runtime);
+
+pub(crate) fn get_tokio_runtime(py: Python) -> PyRef<TokioRuntime> {
+    py.import("synapse._internal")
+        .unwrap()
+        .getattr("runtime")
+        .unwrap()
+        .extract()
+        .unwrap()
+}
+
+pub(crate) fn wait_for_future<F: Future>(py: Python, f: F) -> F::Output
+where
+    F: Send,
+    F::Output: Send,
+{
+    let runtime = &get_tokio_runtime(py).0;
+    py.allow_threads(|| runtime.block_on(f))
 }
---
+++ pysynapse/synapse/__init__.py
@@ -0,0 +1,5 @@
+__all__ = [
+    "Runtime"
+]
+
+from ._internal import Runtime
