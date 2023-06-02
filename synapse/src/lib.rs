223c25778bce11d0841c8a925c0068b326a06734
Dexter Duckworth <dexterduck@users.noreply.github.com>
2023-06-02 12:29:39 -0400

    Updated synapse crate.

--- synapse/src/lib.rs
+++ synapse/src/lib.rs
@@ -1,12 +1,19 @@
+#[cfg(feature = "runtime")]
 pub use synapse_runtime as runtime;
 pub use synapse_tensor as tensor;
+pub use synapse_time as time;
 
 pub use synapse_tensor::{frame, tensor};
+pub use synapse_time::now;
+
+#[cfg(feature = "runtime")]
+pub use runtime::{Runtime, RuntimeConfig};
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error("synapse tensor error")]
     Tensor(#[from] tensor::Error),
+    #[cfg(feature = "runtime")]
     #[error("synapse runtime error")]
     Runtime(#[from] runtime::Error),
 }
