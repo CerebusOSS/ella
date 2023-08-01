8e2a530d55b5a0a2ba7ec28636e19f36aa4de39a
Dexter Duckworth <dexterduck@users.noreply.github.com>
2023-08-01 10:22:30 -0400

    Updated ella-server to not depend on protobuf-src on Windows.

--- ella-server/build.rs
+++ ella-server/build.rs
@@ -1,6 +1,9 @@
 fn main() {
-    std::env::set_var("PROTOC", protobuf_src::protoc());
-    std::env::set_var("PROTOC_INCLUDE", protobuf_src::include());
+    if cfg!(not(target_os = "windows")) {
+        std::env::set_var("PROTOC", protobuf_src::protoc());
+        std::env::set_var("PROTOC_INCLUDE", protobuf_src::include());
+    }
+
     tonic_build::configure()
         .compile(&["api/engine.proto"], &["api/"])
         .unwrap();
