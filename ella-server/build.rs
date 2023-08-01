7e867940d63be53070ff7c1a9257d1d5a5352e65
Dexter Duckworth <dexterduck@users.noreply.github.com>
2023-08-01 11:17:06 -0400

    Fixed incorrect conditional compilation in ella-server.

--- ella-server/build.rs
+++ ella-server/build.rs
@@ -1,8 +1,8 @@
 fn main() {
-    if cfg!(not(target_os = "windows")) {
-        std::env::set_var("PROTOC", protobuf_src::protoc());
-        std::env::set_var("PROTOC_INCLUDE", protobuf_src::include());
-    }
+    #[cfg(not(target_os = "windows"))]
+    std::env::set_var("PROTOC", protobuf_src::protoc());
+    #[cfg(not(target_os = "windows"))]
+    std::env::set_var("PROTOC_INCLUDE", protobuf_src::include());
 
     tonic_build::configure()
         .compile(&["api/engine.proto"], &["api/"])
