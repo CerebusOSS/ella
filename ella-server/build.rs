d1721f1126b3abd2292a566aa7caec79616170be
Dexter Duckworth <dexterduck@users.noreply.github.com>
2023-08-02 13:16:54 -0400

    fix: broken protobuf dependency during cross-compilation
    On certain platforms trying to cross-compile the Python package causes protobuf-src to fail to compile.
    ella and pyella crates now include a `protobuf` feature that can be disabled.
    Python CI workflow now uses system protobuf.

--- ella-server/build.rs
+++ ella-server/build.rs
@@ -1,10 +1,11 @@
 fn main() {
-    #[cfg(not(target_os = "windows"))]
+    #[cfg(all(feature = "protobuf", not(target_os = "windows")))]
     std::env::set_var("PROTOC", protobuf_src::protoc());
-    #[cfg(not(target_os = "windows"))]
+    #[cfg(all(feature = "protobuf", not(target_os = "windows")))]
     std::env::set_var("PROTOC_INCLUDE", protobuf_src::include());
 
     tonic_build::configure()
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile(&["api/engine.proto"], &["api/"])
         .unwrap();
 }
